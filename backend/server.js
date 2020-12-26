94c2427bf0b9f06911c7354be4171c4801bc5af3
alexkulikov101 <bullet_310@mail.ru>
2020-12-26 21:30:32 +0300

    Back End User Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,32 @@
-import express from "express";
-import dotenv from "dotenv";
-import colors from "colors";
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
 
-import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoutes.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
-dotenv.config();
+dotenv.config()
 
-connectDB();
+connectDB()
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("API is running...!");
-});
+app.use(express.json())
 
-app.use("/api/products", productRoutes);
+app.get('/', (req, res) => {
+  res.send('API is running...!')
+})
 
-app.use(notFound);
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
-app.use(errorHandler);
+app.use(notFound)
 
-const PORT = process.env.PORT || 5000;
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
@@ -30,4 +34,4 @@ app.listen(
     `Server running in ${process.env.NODE_ENV} mode  on port ${PORT}`.yellow
       .bold
   )
-);
+)
