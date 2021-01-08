4b87a8330a4ab75d3334488be95ba7c6b73bbb1c
alexkulikov101 <bullet_310@mail.ru>
2021-01-08 18:54:50 +0300

    make product review & search

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,20 +1,22 @@
-import React, { useEffect } from "react";
-import { Col, Row } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 
-import Product from "../components/Product";
-import { listProducts } from "../actions/productActions";
-import Message from "../components/Message";
-import Loader from "../components/Loader";
+import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 
-const HomeScreen = () => {
-  const dispatch = useDispatch();
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+const HomeScreen = ({ match }) => {
+  const dispatch = useDispatch()
+  const keyword = match.params.keyword
+
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword))
+  }, [dispatch, keyword])
 
   return (
     <>
@@ -22,7 +24,7 @@ const HomeScreen = () => {
       {loading ? (
         <Loader />
       ) : error ? (
-        <Message variant="danger">{error}</Message>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
@@ -33,7 +35,7 @@ const HomeScreen = () => {
         </Row>
       )}
     </>
-  );
-};
+  )
+}
 
-export default HomeScreen;
+export default HomeScreen
